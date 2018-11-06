f647f7981651ec880e2ae7b6c886063cff3daa83
Rich <liujh88888@sina.cn>
2018-11-06 22:39:55 +0800

    genesis

---
+++ MusicStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Music> Musics { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Singer> Singers { get; set; }
+    }
+}
